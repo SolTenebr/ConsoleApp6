3706826fe1dfac819844415d73be00caca667ab9
Филипенко Ярослав <filipenko.yaroslav@outlook.com>
2020-11-07 19:09:59 +1000

    Добавление событий

--- ConsoleApp6/Cat.cs
+++ ConsoleApp6/Cat.cs
@@ -8,94 +8,86 @@ namespace ConsoleApp6
    public class Cat
     {
         byte _hungryStatus;
-
         public Cat(string name, DateTime birthday)
         {
             Name = name;
-            Birthday = birthday;
+            BirthDay = birthday;
             Task.Run(LifeCircle);
         }
-
         public string Name
         {
-            get;
-            set;
+            get; set;
         }
-
+        public DateTime BirthDay
+        {
+            get; set;
+        }
         public void MakeNoise()
         {
-            Console.WriteLine($"{Name} мяукает");
+            Console.WriteLine(Name + " мяукает");
         }
-
-        public DateTime Birthday
+        public int GetAge()
         {
-            get;
-            set;
+            return (DateTime.Today - BirthDay).Days / 365;
         }
 
-        public int GatAge()
+        public void GetStatus()
         {
-            return (DateTime.Today - Birthday).Days / 365;
+            Console.WriteLine(Name);
+            Console.WriteLine($"Возраст: {GetAge()}");
+            if (HungryStatus < 10)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кот умерает от голода ");
+            }
+            else if (HungryStatus >= 10 && HungryStatus <= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Кот очень голоден");
+            }
+            else if (HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Кот хочет кушать");
+            }
+            else if (HungryStatus > 70 && HungryStatus <= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Кот не против перекусить");
+            }
+            else if (HungryStatus > 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Кот недавно поел");
+            }
+            Console.ResetColor();
         }
-
+        async Task LifeCircle()
+        {
+            await Task.Delay(1000);
+            HungryStatus -= 10;
+            await LifeCircle();
+        }
+        public void Feed()
+        {
+            HungryStatus = 100;
+        }
+        public event EventHandler HungryStatusChanged;
         public byte HungryStatus
         {
             get { return _hungryStatus; }
             set
             {
                 if (value < 0)
-                {
                     _hungryStatus = 0;
-                }
                 else if (value > 100)
-                {
                     _hungryStatus = 100;
-                }
                 else
                     _hungryStatus = value;
-            }
+                if (_hungryStatus != value)
+                    HungryStatusChanged?.Invoke(this, null);
 
-        }
-
-        public void GetStatus()
-        {
-            Console.WriteLine(Name);
-            Console.WriteLine($"Возраст: {GatAge()}");
-            if (HungryStatus <= 10)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Кошка умирает от голода");
             }
-            else if (HungryStatus > 10 && HungryStatus <= 40)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Кошка очень голодна");
-            }
-            else if (HungryStatus > 70 && HungryStatus <= 90)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Кошка не против перекусить");
-            }
-            else if(HungryStatus > 90)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Кошка не голодна");
-            }
-            Console.ResetColor();
-        }
-
-        async Task LifeCircle()
-        {
-            await Task.Delay(1000);
-            HungryStatus -= 10;
-            GetStatus();
-            if (HungryStatus == 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Кошка умерла :(");
-            }
-            else await LifeCircle();
-
         }
     }
 }
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -6,12 +6,38 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            Cat c = new Cat("ALKATRAZ-Z01Y78", new DateTime(2009, 06, 26));
-            c.MakeNoise();
-            Console.WriteLine($"Кошке по имени {c.Name} уже {c.GatAge()} лет");
-            c.HungryStatus = 150;
-
+            Cat cat = new Cat("ALKATRAZ", new DateTime(2005, 8, 7));
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+            cat.HungryStatus = 150;
+            cat.HungryStatusChanged += Cat_HungryStatusChanged;
+            Cat cat2 = new Cat("Kit", new DateTime(2005, 8, 7));
+            Console.WriteLine($"Коту по имени {cat2.Name} уже {cat2.GetAge()} лет");
+            cat2.HungryStatusChanged += Cat2_HungryStatusChanged;
             Console.ReadLine();
+
         }
+
+        public static void Cat_HungryStatusChanged(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Cat cat = (Cat)sender;
+            if (cat.HungryStatus < 20 && rnd.Next(0, 10) < 5)
+                cat.Feed();
+            else
+                cat.GetStatus();
+        }
+        public static void Cat2_HungryStatusChanged(object sender, EventArgs e)
+
+        {
+            Random rnd = new Random();
+            Cat cat = (Cat)sender;
+            if (cat.HungryStatus < 20 && rnd.Next(0, 10) < 5)
+                cat.Feed();
+            else
+                cat.GetStatus();
+
+
+        }
+
     }
 }
