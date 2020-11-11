f3f631d690a6b119cb1f356277dc01c6f320e872
Филипенко Ярослав <filipenko.yaroslav@outlook.com>
2020-11-11 22:23:13 +1000

    Оптимизация

--- ConsoleApp6/CatSmartHouse.cs
+++ ConsoleApp6/CatSmartHouse.cs
@@ -6,37 +6,69 @@ namespace ConsoleApp6
 {
     class CatSmartHouse
     {
-        public int FoodResource;
-        List<Cat> cats = new List<Cat>();
-        public int foodResource;
-        public CatSmartHouse(int FoodResource)
-        {
-            foodResource = FoodResource;
-        }
-        public int FoodResourse
-        { get; set; }
 
+        List<Cat> cats = new List<Cat>();
+
+        void PrintStatus()
+        {
+            int leftPosition = Console.CursorLeft;
+            int topPosition = Console.CursorTop;
+
+            for (int i = 0; i < cats.Count; i++)
+            {
+                string message = cats[i].GetStatus("");
+                int color = Convert.ToInt32(message.Substring(0, 1));
+                Console.SetCursorPosition(0, i);
+                Console.ForegroundColor = (ConsoleColor)color;
+                Console.Write(message.Substring(2).Trim().PadRight(50));
+                Console.ResetColor();
+
+            }
+            Console.SetCursorPosition(0, CatsCount);
+            Console.Write($"Еды в вольере: + {FoodResourse}".PadRight(50));
+            Console.SetCursorPosition(leftPosition, topPosition);
+        }
+
+
+        public CatSmartHouse(int foodResource)
+        {
+            FoodResource = foodResource;
+        }
         public void AddCat(Cat cat)
         {
             cats.Add(cat);
             cat.HungryStatusChanged += Cat_HungryStatusChanged;
         }
-        private void Cat_HungryStatusChanged(object sender, EventArgs e)
+        public int FoodResource
         {
-            var cat = (Cat)sender;
-            if (cat.HungryStatus <= 20 && FoodResourse > 0)
+            get;
+            set;
+        }
+        public int CatsCount
+        {
+            get
             {
-                byte needFood = (byte)(100 - cat.HungryStatus);
-                if (FoodResourse > needFood)
-                    FoodResourse -= needFood;
-                else
-                {
-                    needFood = (byte)FoodResourse;
-                    FoodResourse = 0;
-                }
-                cat.Feed(needFood);
-                Console.WriteLine($"Покормлена кошка: {cat.Name}\nОстаток еды в вольере: {FoodResourse}");
+                return cats.Count;
             }
         }
+        public int FoodResourse { get; set; }
+
+        public void Cat_HungryStatusChanged(object sender, EventArgs e)
+        {
+            var cat = (Cat)sender;
+            if (cat.HungryStatus <= 20 && FoodResource > 0)
+            {
+                byte needFood = (byte)(100 - cat.HungryStatus);
+                if (FoodResource > needFood)
+                    FoodResource -= needFood;
+                else
+                {
+                    needFood = (byte)FoodResource;
+                    FoodResource = 0;
+                }
+                cat.Feed(needFood);
+                PrintStatus();
+            }
+        }
+      }
     }
-}
